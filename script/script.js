6fbdb3886277a57313e6cb53ab78af3ce285c750
lochanathiwanka <lochanathiwanka62@gmail.com>
2021-06-09 16:53:19 +0530

    Updated

--- script/script.js
+++ script/script.js
@@ -62,11 +62,13 @@ matches();
 
 let condition = false;
 $('#btnMenu').click(function () {
+    let slide_menu_container = $('#slide-menu-container');
     let slide_menu = $('#slide-menu');
     let body = $('body');
 
     if (condition === false) {
         condition = true;
+        slide_menu_container.css("display", "block");
         slide_menu.css("display", "block");
         slide_menu.animate({
             "right": "-100px"
@@ -74,10 +76,10 @@ $('#btnMenu').click(function () {
         body.css("overflow", "hidden");
     } else {
         condition = false;
+        slide_menu_container.css("display", "none");
         slide_menu.animate({
             right: "-340px"
         }, 100, 'linear', function () {
-            /*slide_menu.hide(100);*/
             slide_menu.css("display", "none");
         });
         body.css("overflow", "auto");
@@ -85,9 +87,11 @@ $('#btnMenu').click(function () {
 });
 
 $('#slide-menu > #menu-icon > li').click(function () {
+    let slide_menu_container = $('#slide-menu-container');
     let slide_menu = $('#slide-menu');
     let body = $('body');
     condition = false;
+    slide_menu_container.css("display", "none");
     slide_menu.animate({
         right: "-340px"
     }, 100, 'linear', function () {
